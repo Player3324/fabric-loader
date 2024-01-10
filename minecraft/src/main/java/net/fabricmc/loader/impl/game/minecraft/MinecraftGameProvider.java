ef08d5ea2ba14828acd452c96f3e6110e4a334c8
Player <player@player.to>
2024-01-10 16:11:12 -0500

    rework class transformation

--- minecraft/src/main/java/net/fabricmc/loader/impl/game/minecraft/MinecraftGameProvider.java
+++ minecraft/src/main/java/net/fabricmc/loader/impl/game/minecraft/MinecraftGameProvider.java
@@ -38,6 +38,7 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.loader.api.ObjectShare;
 import net.fabricmc.loader.api.VersionParsingException;
 import net.fabricmc.loader.api.extension.ModMetadataBuilder.ModDependencyBuilder;
+import net.fabricmc.loader.api.extension.transform.ClassTransformer;
 import net.fabricmc.loader.api.metadata.ModDependency;
 import net.fabricmc.loader.impl.FabricLoaderImpl;
 import net.fabricmc.loader.impl.FormattedException;
@@ -48,10 +49,10 @@ import net.fabricmc.loader.impl.game.minecraft.patch.BrandingPatch;
 import net.fabricmc.loader.impl.game.minecraft.patch.EntrypointPatch;
 import net.fabricmc.loader.impl.game.minecraft.patch.EntrypointPatchFML125;
 import net.fabricmc.loader.impl.game.minecraft.patch.TinyFDPatch;
-import net.fabricmc.loader.impl.game.patch.GameTransformer;
 import net.fabricmc.loader.impl.launch.FabricLauncher;
 import net.fabricmc.loader.impl.launch.MappingConfiguration;
 import net.fabricmc.loader.impl.metadata.BuiltinModMetadata;
+import net.fabricmc.loader.impl.transformer.ClassTransformHandler;
 import net.fabricmc.loader.impl.util.Arguments;
 import net.fabricmc.loader.impl.util.ExceptionUtil;
 import net.fabricmc.loader.impl.util.LoaderUtil;
@@ -87,12 +88,6 @@ public class MinecraftGameProvider implements GameProvider {
 	private Collection<Path> validParentClassPath; // computed parent class path restriction (loader+deps)
 	private McVersion versionData;
 	private boolean hasModLoader = false;
-
-	private final GameTransformer transformer = new GameTransformer(
-			new EntrypointPatch(this),
-			new BrandingPatch(),
-			new EntrypointPatchFML125(),
-			new TinyFDPatch());
 
 	@Override
 	public String getGameId() {
@@ -392,7 +387,11 @@ public class MinecraftGameProvider implements GameProvider {
 
 		setupLogHandler(launcher, true);
 
-		transformer.locateEntrypoints(launcher, gameJars);
+		ClassTransformer<?> entrypointPatch = EntrypointPatch.create(this);
+		if (entrypointPatch != null) ClassTransformHandler.addInternalTransformer(entrypointPatch);
+		ClassTransformHandler.addInternalTransformer(BrandingPatch.create());
+		ClassTransformHandler.addInternalTransformer(EntrypointPatchFML125.create());
+		ClassTransformHandler.addInternalTransformer(TinyFDPatch.create());
 	}
 
 	private void setupLogHandler(FabricLauncher launcher, boolean useTargetCl) {
@@ -455,11 +454,6 @@ public class MinecraftGameProvider implements GameProvider {
 		if (writeIdx < ret.length) ret = Arrays.copyOf(ret, writeIdx);
 
 		return ret;
-	}
-
-	@Override
-	public GameTransformer getEntrypointTransformer() {
-		return transformer;
 	}
 
 	@Override
